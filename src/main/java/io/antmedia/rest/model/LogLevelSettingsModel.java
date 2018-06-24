f016e8a3b379c5a97ee24c47ad799fe02e2ad3be
Selim Emre <selimemretoy@gmail.com>
2018-06-24 15:14:42 +0300

    log level Setting Model Added.

---
+++ src/main/java/io/antmedia/rest/model/LogLevelSettingsModel.java
@@ -0,0 +1,22 @@
+package io.antmedia.rest.model;
+
+public class LogLevelSettingsModel {
+	
+	private String logFileSize;
+	private String logLevel;
+	
+	public String getLogFileSize() {
+		return logFileSize;
+	}
+	public void setLogFileSize(String logFileSize) {
+		this.logFileSize = logFileSize;
+	}
+	
+	public String getLogLevel() {
+		return logLevel;
+	}
+	public void setLogLevel(String logLevel) {
+		this.logLevel = logLevel;
+	}
+
+}
